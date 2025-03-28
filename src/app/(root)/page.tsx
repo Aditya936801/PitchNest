cd765287e8fda1dfed42c551fab8ef106ba417ec
Aditya <singhkaditya22@gmail.com>
2025-03-29 02:04:00 +0530

    startup card created and search implemented on home page

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,6 +1,12 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard, { StartupCardType } from "@/components/StartupCard";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }> }) {
   const query = (await searchParams).query;
+  const params = { search: query || null };
+  const { data } = await sanityFetch({ query: STARTUPS_QUERY, params });
+  const posts = data as StartupCardType[];
   return (
     <>
       <section className="pink_container">
@@ -13,8 +19,15 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ q
       </section>
       <section className="section_container">
         <p className="text-30-semibold">{query ? `Search results for "${query}"` : "All Startups"}</p>
-        <ul className="mt-7 card_grid"></ul>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts?.map((post: StartupCardType) => <StartupCard key={post?._id} post={post} />)
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
+      <SanityLive />
     </>
   );
 }
